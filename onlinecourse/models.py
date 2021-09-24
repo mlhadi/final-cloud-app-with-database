43bd5953a357e10577297727178dfe3ad23d7b23
= <=>
2021-09-24 12:07:07 +0000

    Added missing argument.

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -103,7 +103,7 @@ class Enrollment(models.Model):
     # Other fields and methods you would like to design
 class Question(models.Model):
     # Foreign key to lesson
-    lesson = models.ForeignKey(Lesson)
+    lesson = models.ForeignKey(Lesson, on_delete=models.CASCADE)
     # question text
     question_text = models.TextField()
     # question grade/mark
